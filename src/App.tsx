f40f5d34698292d47dc87fd33f1f0084866a0af4
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-16 20:10:24 -0300

    Desenvolver as funções e os componentes no react

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
 
 
 
 export function App() {
   
   return (
-    <>
+    <div>
       Ola      
-    </>
+    </div>
   )
 }
 
+//Função JS
+const teste = () => {
+  return 1 + 1;
+}
 
+//Função JS
+const useTeste = () => {
+  return 1 + 1;
+}
+
+//React hook
+const useTest = () => {
+  const [value] = useState(1 + 1);
+  return value;
+}
+
+//Componente funcional React
+const Test = () => {
+  return (
+    <div>teste</div>
+  )
+}
+
+//Função JS que retorna html react
+const test = () => {
+  return (
+    <div>teste</div>
+  )
+}
