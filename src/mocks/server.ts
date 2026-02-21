80f3f9302c4028b6a7c4cf96f89140968ffe3a6d
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-02-21 16:12:45 -0300

    Desenvolver as rotas como get, post, put e delete

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -10,6 +10,31 @@ createServer({
               return [
                 { id: 1, label: 'Learn MirageJS', completed: false },
               ]
-         });  
+        }); 
+         
+         this.post('/todos', (schema, request) => {
+            let attrs = JSON.parse(request.requestBody);
+            const todo = schema.create(attrs, attrs);
+            return todo;
+        });
+
+        this.put('/todos/:id', (schema, request) => {
+            let id = request.params.id;
+            let  newAttrs = JSON.parse(request.requestBody);
+            
+            let todo = schema.find('todos',id);
+            todo?.update(newAttrs);
+            return {};
+        });
+
+
+        this.delete('/todos/:id', (schema, request) => {
+            let id = request.params.id;
+            
+            let todo = schema.find('todos',id);
+            todo?.destroy();
+            
+            return {};
+        });
     },
 })
